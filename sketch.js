85f3193bd4b8e44255074dab7ad32c47ec993fac
EmilL22r <emilvirkelystlindholm@gmail.com>
2024-05-14 15:26:10 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,24 +2,33 @@ let sound;
 
 function preload()
 {
-  soundFormats('mp3', 'ogg')
+  
+  soundFormats('mp3', 'ogg');
   vineboom = loadSound('assets/vineboom.mp3')
+  
 }
 
 function setup() 
 {
+  
   cnv = createCanvas(400, 400);
   sound = new VisualSound(vineboom, 0, 0 ,0);
 
   cnv.mousePressed(canvasPressed);
+
+  keyboard = new Piano(5,5,1);
 }
 
 function draw() 
 {
   background(220);
+
+  keyboard.VisualKeys();
+
 }
 
 function canvasPressed()
 {
   sound.play();
 }
+
